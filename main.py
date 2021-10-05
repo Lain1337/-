934e70daa04bee04d9a5856c15cde01e7a23298f
Lain1337 <prosergey2000@mail.ru>
2021-10-05 12:31:04 +0300

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 a = input('Введите первое число: ')
 b = input('Введите второе число: ')
-c = input('Выберите действие:\n1-сложение\n2-вычитание\n3-умножение\n4-деление')
+c = input('Выберите действие:\n1-сложение\n2-вычитание\n3-умножение\n4-деление\n')
 
-def add(x,y):
+def add(x, y):
     z = int(x) + int(y)
     return z
-def sub(x,y):
+def sub(x, y):
     z = int(x) - int(y)
     return z
-def mul(x,y):
+def mul(x, y):
     z = int(x) * int(y)
     return z
-def div(x,y):
+def div(x, y):
     z = int(x) / int(y)
     return z
+
 if c == '1':
-    result=add(a,b)
-    print(str(a) + '/' + str(b) + '=' + str(result))
+    result = add(a, b)
+    print(str(a)+'/'+str(b)+'='+str(result))
 elif c == '2':
-    result=sub(a,b)
-    print(str(a) + '-' + str(b) + '=' + str(result))
+    result = sub(a, b)
+    print(str(a)+'-'+str(b)+'='+str(result))
 elif c == '3':
-    result=mul(a,b)
-    print(str(a) + 'x' + str(b) + '=' + str(result))
+    result = mul(a, b)
+    print(str(a)+'x'+str(b)+'='+str(result))
 elif c == '4':
-    result=div(a,b)
-    print(str(a) + '/' + str(b) + '=' + str(result))
+    result = div(a, b)
+    print(str(a)+'/'+str(b)+'='+str(result))
 else:
     print('Нет такого действия(')
 
