8b4996e9c9c342171739e5ad39267df29bfff241
Lain1337 <91277581+Lain1337@users.noreply.github.com>
2021-12-04 16:56:57 +0300

    Add files via upload

--- notation.py
+++ notation.py
@@ -1,111 +1,118 @@
-string = input('Ввод ')
-stack0 = list()
-stack1 = list()
-num = list()
-operators = {'+': 1, '-': 2, '*': 3, '/': 4}
-operators1 = {'+': 1, '-': 2, '*': 3, '/': 4, '(': 0}
-numbers = list()
-def converting(string):
-    for i in range(0, len(string)):
-        if (string[i].isdigit()) or (string[i] == '.'):
-            num.append(string[i])
-        elif string[i] in operators:
-            stack0.append("".join(map(str, num)))
-            list(num)
-            num.clear()
+def go():
+    string = input('Ввод ')
 
-            if stack1:
-                if (operators.get(string[i])) > (operators1.get(stack1[-1])):
-                    stack1.append(string[i])
-                elif int(operators.get(string[i])) <= int(operators.get(stack1[-1])):
-                    stack0.append(stack1.pop())
-                    stack1.append(string[i])
-            else:
-                stack1.append(string[i])
-        elif string[i] == '(':
-            if num:
+    def converting(string):
+        stack0 = list()
+        stack1 = list()
+        num = list()
+        operators = {'+': 1, '-': 2, '*': 3, '/': 4}
+        operators1 = {'+': 1, '-': 2, '*': 3, '/': 4, '(': 0}
+        for i in range(0, len(string)):
+            if (string[i].isdigit()) or (string[i] == '.'):
+                num.append(string[i])
+            elif string[i] in operators:
                 stack0.append("".join(map(str, num)))
                 list(num)
                 num.clear()
-                stack1.append(string[i])
-            else:
-                stack1.append(string[i])
-        elif string[i] == ')':
-            stack0.append("".join(map(str, num)))
-            list(num)
-            num.clear()
-            k = 0
-            stack1.reverse()
-            while k == 0:
-                if stack1[k] == '(':
-                    k += 1
-                else:
-                    stack0.append(stack1.pop(k))
-            stack1.pop(0)
-            stack1.reverse()
-    stack0.append("".join(map(str, num)))
-    list(num)
-    num.clear()
-    while stack1:
-        stack0.append(stack1.pop())
-        print(stack0)
-    k = 0
-    return stack0
 
-def counting(stack0):
-    k = 0
-    for i in range(0, len(stack0)):
-        if (stack0[i] not in operators) and (stack0[i] != ''):
-            numbers.append(float(stack0[i]))
-            k += 1
-        elif stack0[i] == '+':
-            if k > 2:
-                a = sum(numbers)
-                numbers.clear()
+                if stack1:
+                    if (operators.get(string[i])) > (operators1.get(stack1[-1])):
+                        stack1.append(string[i])
+                    elif int(operators.get(string[i])) <= int(operators.get(stack1[-1])):
+                        stack0.append(stack1.pop())
+                        stack1.append(string[i])
+                else:
+                    stack1.append(string[i])
+            elif string[i] == '(':
+                if num:
+                    stack0.append("".join(map(str, num)))
+                    list(num)
+                    num.clear()
+                    stack1.append(string[i])
+                else:
+                    stack1.append(string[i])
+            elif string[i] == ')':
+                stack0.append("".join(map(str, num)))
+                list(num)
+                num.clear()
+                k = 0
+                stack1.reverse()
+                while k == 0:
+                    if stack1[k] == '(':
+                        k += 1
+                    else:
+                        stack0.append(stack1.pop(k))
+                stack1.pop(0)
+                stack1.reverse()
+        stack0.append("".join(map(str, num)))
+        list(num)
+        num.clear()
+        while stack1:
+            stack0.append(stack1.pop())
+        k = 0
+        return stack0
+
+    def counting(stack2):
+        numbers = list()
+        operators = {'+': 1, '-': 2, '*': 3, '/': 4}
+        k = 0
+        for i in range(0, len(stack2)):
+            if (stack2[i] not in operators) and (stack2[i] != ''):
+                numbers.append(float(stack2[i]))
+                k += 1
+            elif stack2[i] == '+':
+                if k > 2:
+                    a = sum(numbers)
+                    numbers.clear()
+                    numbers.append(a)
+                else:
+                    op1 = numbers.pop()
+                    op2 = numbers.pop()
+                    a = op1 + op2
+                    numbers.append(a)
+                k = 0
+            elif stack2[i] == '-':
+                if k > 2:
+                    a = numbers[0] - sum(numbers[1:])
+                    numbers.clear()
+                    numbers.append(a)
+                else:
+                    op1 = numbers.pop()
+                    op2 = numbers.pop()
+                    a = op2 - op1
+                    numbers.append(a)
+                k = 0
+            elif stack2[i] == '*':
+                if k > 2:
+                    for i2 in range(1, len(numbers)):
+                        a = numbers[0]
+                        a *= numbers[i2]
+                    numbers.clear()
+                    numbers.append(a)
+                else:
+                    op1 = numbers.pop()
+                    op2 = numbers.pop()
+                    a = op1 * op2
                 numbers.append(a)
-            else:
-                op1 = numbers.pop()
-                op2 = numbers.pop()
-                a = op1 + op2
-                numbers.append(a)
-            k = 0
-        elif stack0[i] == '-':
-            if k > 2:
-                a = numbers[0] - sum(numbers[1:])
-                numbers.clear()
-                numbers.append(a)
-            else:
-                op1 = numbers.pop()
-                op2 = numbers.pop()
-                a = op2 - op1
-                numbers.append(a)
-            k = 0
-        elif stack0[i] == '*':
-            if k > 2:
-                for i2 in range(1, len(numbers)):
-                    a = numbers[0]
-                    a *= numbers[i2]
-                numbers.clear()
-                numbers.append(a)
-            else:
-                op1 = numbers.pop()
-                op2 = numbers.pop()
-                a = op1 * op2
-            numbers.append(a)
-            k = 0
-        elif stack0[i] == '/':
-            if k > 2:
-                for l in range(1, len(numbers)):
-                    a = numbers[0]
-                    a /= numbers[l]
-                numbers.clear()
-                numbers.append(a)
-            else:
-                op1 = numbers.pop()
-                op2 = numbers.pop()
-                a = op2 / op1
-                numbers.append(a)
-            k = 0
-    a = numbers.pop()
-    return a
-print(counting(converting(string)))
+                k = 0
+            elif stack2[i] == '/':
+                if k > 2:
+                    for l in range(1, len(numbers)):
+                        a = numbers[0]
+                        a /= numbers[l]
+                    numbers.clear()
+                    numbers.append(a)
+                else:
+                    op1 = numbers.pop()
+                    op2 = numbers.pop()
+                    a = op2 / op1
+                    numbers.append(a)
+                k = 0
+        a = numbers.pop()
+        return a
+
+    print(string + '=' + str(counting(converting(string))))
+
+
+if __name__ == '__main__':
+    go()
